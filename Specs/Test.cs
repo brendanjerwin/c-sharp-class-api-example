a2588c9486a8916ca27389a7d94f96a50b020894
Brendan Erwin <brendanjerwin@gmail.com>
2014-02-26 08:58:47 -0500

    Initial

---
+++ Specs/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Specs
+{
+  [TestFixture ()]
+  public class Test
+  {
+    [Test ()]
+    public void TestCase() {
+      Assert.AreEqual (true, false);
+    }
+  }
+}
+
---
+++ c-sharp-class-api-example/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace csharpclassapiexample
+{
+    public class PriceResult
+    {
+        public Decimal Amount { get; set; }
+
+        public string Currency { get; set; }
+    }
+
+    class MainClass
+    {
+        static RestClient client;
+
+        private static void Configure()
+        {
+            client = new RestClient("https://coinbase.com/api/v1/");
+            client.AddDefaultHeader("API_ACCESS_TOKEN", "asdfjhsdkfjhsdakjhfds");
+            client.AddDefaultHeader("CLIENT_VERSION", "asdfsfdsf");
+        }
+
+        private static PriceResult GetCurrentPrice()
+        {
+            var request = new RestRequest("prices/spot_rate", Method.GET);
+            request.Timeout = 10;
+
+            return client.Execute<PriceResult>(request).Data;
+        }
+
+        public static void Main(string[] args)
+        {
+            Configure();
+            while (true)
+            {
+                var price = GetCurrentPrice();
+                Console.WriteLine("Price: {0}", price.Amount);
+                System.Threading.Thread.Sleep(1 * 1000);
+            }
+        }
+    }
+}
